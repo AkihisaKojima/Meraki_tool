01d463cdffb026c3096af2f4d7ba62f1e59f4078
AkihisaKojima <mingshouxiaodao133@gmail.com>
2020-02-20 11:39:47 +0900

    add keyfiles

--- server.py
+++ server.py
@@ -4,11 +4,16 @@ import pprint
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET','POST'])
+KEY_PATH = "keys/Meraki_API.txt"
+with open(KEY_PATH) as f:
+	api_key = f.read()
+
+
+@app.route('/', methods=['GET'])
 def root():
-	return render_template('main.html', HELLO='hello')
+	return render_template('main.html', HELLO='Good!')
 
 
 if __name__ == '__main__':
-#	app.run(host="0.0.0.0" ,ssl_context=('openssl/server.crt', 'openssl/server.key'), debug=True)
-	app.run(host="0.0.0.0" ,debug=True)
+	app.run(host="0.0.0.0" ,ssl_context=('openssl/server.crt', 'openssl/server.key'), debug=True)
+#	app.run(host="0.0.0.0" ,debug=True)
